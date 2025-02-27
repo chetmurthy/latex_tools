00cb832d1de970a7137499432597cb98b662297b
Chet Murthy <chetsky@gmail.com>
2025-02-26 19:22:07 -0800

    meh

--- tests/latex_token_test.ml
+++ tests/latex_token_test.ml
@@ -1,4 +1,4 @@
-(**pp -syntax camlp5o -package pa_ppx.runtime,pa_ppx.runtime_fat,pa_ppx.testutils,latex_tools,bos *)
+(**pp -syntax camlp5o -package pa_ppx_regexp,pa_ppx.runtime,pa_ppx.runtime_fat,pa_ppx.testutils,latex_tools,bos *)
 open OUnit2
 open Pa_ppx_testutils
 open Latex_tools
@@ -8,6 +8,13 @@ open Latex_lexer
 Pa_ppx_runtime.Exceptions.Ploc.pp_loc_verbose := true ;;
 Pa_ppx_runtime_fat.Exceptions.Ploc.pp_loc_verbose := true ;;
 
+let python_quote s =
+  let b = Buffer.create (String.length s) in
+  s |> String.iter (fun c ->
+           if c = '"' then Buffer.add_char b c
+           else Buffer.add_string b (Char.escaped c)) ;
+  Buffer.contents b
+
 let list_of_tokens_eof lexbuf =
   let rec lrec acc =
     match token lexbuf with
@@ -15,13 +22,15 @@ let list_of_tokens_eof lexbuf =
     | t -> lrec ((List.rev t) @ acc)
   in lrec []
 
+let fmt1 (t,s,_) =
+  let s = Fmt.(str "(%a, %a)@." (quote ~mark:"'" string) (python_quote s) (quote ~mark:"'" pp) t) in
+  [%subst {|\n|} / "" / g m] s
+
 let latex_tokens fname =
   let lexbuf = Sedlexing.Utf8.from_channel (open_in fname) in
   let toks = list_of_tokens_eof lexbuf in
-  toks |> List.iter (fun (t, s, _) ->
-              Fmt.(pf stdout "(%a, %a)@." (quote ~mark:"'" string) (String.escaped s) (quote ~mark:"'" pp) t) ;
-            ) ;
-  Fmt.(pf stdout "@.")
+  let l = List.map fmt1 toks in
+  Fmt.(pf stdout "%a@." (list ~sep:(const string "\n") string) l)
 
 let _ = 
 if not !Sys.interactive then
