37cef9ccad54e227ff8cba5ba414ba9ebbff5449
Chet Murthy <chetsky@gmail.com>
2025-02-27 10:12:56 -0800

    meh

--- tests/latex_token_test.ml
+++ tests/latex_token_test.ml
@@ -1,6 +1,7 @@
-(**pp -syntax camlp5o -package pa_ppx_regexp,pa_ppx.runtime,pa_ppx.runtime_fat,pa_ppx.testutils,latex_tools,bos *)
+(**pp -syntax camlp5o -package pa_ppx_regexp,pa_ppx.runtime,pa_ppx.runtime_fat,pa_ppx.testutils,pa_ppx.utils,latex_tools,bos *)
 open OUnit2
 open Pa_ppx_testutils
+open Pa_ppx_utils
 open Latex_tools
 open Latex_tokens
 open Latex_lexer
@@ -45,6 +46,22 @@ let latex_tokens ~roundtrip fname =
     let l = List.map fmt1 toks in
     Fmt.(pf stdout "%a@." (list ~sep:(const string "\n") string) l)
 
+let stream_map f strm =
+  let rec srec = parser
+    [< 't ; strm >] -> [< 'f t ; srec strm >]
+  | [< >] -> [< >]
+  in srec strm
+
+let latex_tokens' ~roundtrip fname =
+  let lexbuf = Sedlexing.Utf8.from_channel (open_in fname) in
+  let strm = stream_of_tokens Latex_lexer.token lexbuf in
+  if roundtrip then
+    strm |> Stream.iter (fun {text=s} -> print_string s)
+  else
+    let strm = stream_map fmt1 strm in
+    let l = Std.list_of_stream strm in
+    Fmt.(pf stdout "%a@." (list ~sep:(const string "\n") string) l)
+
 let _ = 
 if not !Sys.interactive then
   let fname = ref "" in
@@ -56,7 +73,7 @@ if not !Sys.interactive then
            (fun s -> fname := s)
            "latex_token_test.exe <fname>");
     let roundtrip = !roundtrip in
-    latex_tokens ~roundtrip !fname
+    latex_tokens' ~roundtrip !fname
   end
 else ()
 
